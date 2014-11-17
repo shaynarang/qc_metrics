d84a9fa1916d17445b575f35432050176b9b2cc0
shaynarang <shaynarang@gmail.com>
2014-11-17 10:27:07 -0600

    install haml

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module QcMetrics
     # config.i18n.default_locale = :de
 
     config.generators do |g|
+      g.template_engine :haml
       g.assets          false
       g.helper          false
       g.test_framework  :rspec
